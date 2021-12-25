0564f27b1ca87214fb25bb618e4d82140f7ebe2a
vc64 <vchen2022@spyponders.com>
2021-12-24 23:45:48 -0500

    initial commit

---
+++ aristocrat.py
@@ -0,0 +1,12 @@
+import random
+
+
+# todo
+# look into where to get quotes, maybe litquotes
+# make sure to filter out quotes that are too short
+# maybe just use quotes from specific people, avoids potential content issues
+# good quote sources
+#   jane austen
+#   isaac asimov
+
+# Bartlett's familiar quotations - book
